c8a18133291a7dd53e0522705bf55bf550f67094
Naveen <pothurajunaveen963@gmail.com>
2023-11-22 12:05:10 +0000

    need to pass 1 tc

---
+++ src/components/Feedback/index.js
@@ -0,0 +1,57 @@
+// Write your React code here.
+import {Component} from 'react'
+import './index.css'
+
+class Feedback extends Component {
+  state = {isGiven: false}
+
+  feedbackGiven = () => {
+    this.setState(pervState => ({isGiven: !pervState.isGiven}))
+  }
+
+  render() {
+    const {resources} = this.props
+    const {emojis, loveEmojiUrl} = resources
+    const {isGiven} = this.state
+    console.log(isGiven)
+
+    return (
+      <div className="bg-container1">
+        <div className={`bg-container2 ${''}`}>
+          <h1 className="heading">
+            How satisfied are you with our customer support performance
+          </h1>
+          <ul className="emoji-container">
+            {emojis.map(each => (
+              <li className="emoji-list">
+                <button
+                  className="button"
+                  type="button"
+                  onClick={this.feedbackGiven}
+                  key={each.id}
+                >
+                  <img
+                    src={each.imageUrl}
+                    alt={each.name}
+                    className="emoji-img"
+                  />
+                  <p className="eomji-name">{each.name}</p>
+                </button>
+              </li>
+            ))}
+          </ul>
+        </div>
+        <div className={`bg-container2 ${''}`}>
+          <img src={loveEmojiUrl} alt="love emoji" className="love-img" />
+          <h1 className="heading">Thank you!</h1>
+          <p className="bottom-text">
+            We will use your feedback to improve our customer support
+            performance.
+          </p>
+        </div>
+      </div>
+    )
+  }
+}
+
+export default Feedback
